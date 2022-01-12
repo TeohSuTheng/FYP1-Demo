0cdad6462918dbd52e7ed80687b272c82c3daf8c
Teoh Su Theng <tsthengteoh@hotmail.com>
2022-01-12 13:55:15 +0800

    revert settings.py

--- plantsite/settings.py
+++ plantsite/settings.py
@@ -139,7 +139,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 #STATICFILES_DIRS = [ 
 #    os.path.join(BASE_DIR,'static') 
@@ -159,25 +163,10 @@ LOGOUT_REDIRECT_URL = 'home'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-
 
 # Prevent files with same name from being overwritten
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400',}
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static'),]
-
-AWS_LOCATION = 'static'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
