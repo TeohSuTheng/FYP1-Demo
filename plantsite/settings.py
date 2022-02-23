fe3dbb8cbab00acf350d5d15e2a7a500aa75b748
Teoh Su Theng <tsthengteoh@hotmail.com>
2022-02-23 22:11:23 +0800

    update settings.py

--- plantsite/settings.py
+++ plantsite/settings.py
@@ -23,7 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-$*)jr&e)z*ukepwmmz)ozy)vz)i@g*%l%n11m_(2ersny)hz2p'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
